96e787ac5d25b803ea62b885bfcd4a7d6d7872a0
tkdgns07 <tkdgns070122@gmail.com>
2024-05-13 13:44:29 +0900

    first commit

---
+++ next-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+
+NOTION_PAGE_ID = "295097b33dd049ba8a8e2b21b172bf2d"
